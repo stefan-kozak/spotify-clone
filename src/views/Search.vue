7e60aaff4dbf873b2fa00497e8a587423ee1eb36
stefan-kozak <stefan.kozak99@gmail.com>
2021-07-05 00:08:01 +0200

    added top artist and top track from search

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -9,28 +9,33 @@
     </form>
 
       <a href="#" class="top-search">
-        <h1>Top Search</h1>
-        {{ tracks[0]}}
+        <h1>Top Artist Result</h1>
+        <section class="top-result" v-if="checkArray(artists)">
+          <article>
+            <div>
+              <img :src="artists[0].images[0].url" alt="artist_image">
+            </div>
+            <h1>{{ artists[0].name }}</h1>
+            <p>{{ artists[0].type }}</p>
+          </article>
+        </section>
+      </a>
+
+      <a href="#" class="top-search">
+        <h1>Top Track Result</h1>
+        <section class="top-result" v-if="checkArray(tracks)">
+          <article>
+            <div>
+              <img :src="tracks[0].album.images[0].url" alt="artist_image">
+            </div>
+            <h1>{{ tracks[0].name }}</h1>
+            <p>{{ tracks[0].type }}</p>
+          </article>
+        </section>
       </a>
 
 
-    <ul>
-      <li v-for="track in tracks" :key="track.id">
 
-
-
-        <img :src="track.albumCover.images[0].url" alt="track_album">
-        <div>
-          <a href="#track"><h1>{{ songify(track) }}</h1></a>
-          <a href="#author">{{ track.artist[0].name }}</a>
-        </div>
-
-      
-
-        
-        
-      </li>
-    </ul>
 
   </div>
 </template>
@@ -44,15 +49,16 @@ export default {
   data() {
     return {
       query: '',
-      tracks: []
+      tracks: [],
+      artists: [],
     }
   },
   methods: {
     getMusic(){
       let query = encodeURI(this.query)
 
-       // GET TOKEN AT URL 'https://accounts.spotify.com/authorize?client_id=60ab2a632f3942debcf22cd7da0c3f81&redirect_uri=https%3A%2F%2Fgrady.sk%2F&scope=user-read-private%20user-read-email&response_type=token&state=123'
-      let accessToken = 'BQDlv6LHuBX6-FLdt1moWIbs4Vwl94UQaW5dcG43X60vwse7OClqqNh6tE4kSaGheCr63ChoAPNNFLMOFnON3Cqr5Hq2Wbx6RmOBUPSN_z19EbuJvtJ57WlO4iAPC88zgfQpwH8MvSCXW92W-6-GyE7dIrpZgkQfNjU-ZAc9IsEVzvmPTJQ&token_type=Bearer&expires_in=3600&state=123';
+       // GET TOKEN AT URL 'https://accounts.spotify.com/authorize?client_id=60ab2a632f3942debcf22cd7da0c3f81&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2F&scope=user-read-private%20user-read-email&response_type=token&state=123'
+      let accessToken = 'BQBfWychjkbs9hS8btMdv7ktmHL0bavfGm4l5ZEy323xp4_ddFESdrMvH2PzxpjLr2o033Sq7h00-_cXlP-E3UbIruoJRP4dnud8DV_zEJLr8NRSGpDvRpPPgRg01OiwvRQ1wPy9H74WuAbz-KJYvzSccFBbDVHhLX7Looe9-XBLZJtsDFE&token_type=Bearer&expires_in=3600&state=123';
 
 
       const config = {
@@ -66,32 +72,50 @@ export default {
       //let res = axios(config);
       axios.get(config.url, config)
         .then(res => {
+          //CLEAN TRACKS FOR NEW SEARCH RESULT
           this.tracks = []
-          //console.log(res.data.tracks);
+          //PUSH ALL SEARCHED TRACKS INTO ARRAY
           res.data.tracks.items.forEach( track => {
-            console.log(track);
-            this.extractData(track)
+            this.tracks.push(this.extractTrackData(track))
+          });
 
-            this.tracks.push(this.extractData(track))
+          //CLEAN ARTISTS FOR NEW SEARCH RESULT
+          this.artists = []
+          //PUSH ALL SEARCHED ARTISTS INTO ARRAY
+          res.data.artists.items.forEach( artist => {
+            //console.log(artist)
+            this.artists.push(this.extractArtistsData(artist))
           });
         })
+        // CHECK README IF UNAUTHORIZED
         .catch( error => {
-          console.log(error);
+          console.log(error, "Check readme for authorization");
         })
     },
-    songify(track) {
-        return track.trackName.charAt(0).toUpperCase() + track.trackName.slice(1)// + ' - ' + track.trackName.charAt(0).toUpperCase() + track.trackName.slice(1)
-    },
-    extractData({
+    extractTrackData({
       id,
-      artists: artist,
+      artist,
       uri: audioUri,
-      album: albumCover,
-      name: trackName,
-      album: albumName,
+      album,
+      name,
+      type,
     }) 
     {
-        return  { trackName, id, artist, audioUri, albumCover, albumName } ;  
+        return  { name, id, artist, audioUri, album, type } ;  
+    },
+
+     extractArtistsData({
+      id,
+      name,
+      uri,
+      images,
+      type,
+    }) 
+    {
+        return  { name, id, uri, images, type} ;  
+    },
+    checkArray(a){
+    return a.length > 0 && Array.isArray(a)
     }
   },
 }
@@ -109,6 +133,10 @@ $white: #fff;
 
 //   FONT SIZES
 $f-small: 14px;
+
+*{
+  text-decoration: none;
+}
 
 .search{
   background-color: darken($secondary-color, 50%);
@@ -152,6 +180,54 @@ form{
     }
   }
 
+}
+
+.top-result{
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  width: 35%;
+  padding: 2em;
+  padding-left: 3em;
+  margin-bottom: 2em;
+  background-color: $secondary-color;
+  color: $white;
+
+  
+  article{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    h1{
+      font-size: 1.5em;
+      padding-top: 0.5em;
+      margin-bottom: 0.5em;
+    }
+    p{
+      font-size: 0.8em;
+      text-transform: uppercase;
+      font-weight: bold;
+      letter-spacing: 1px;
+      background-color: #000;
+      padding: 0.5em 1em;
+      border-radius: 9999px;
+    }
+    div{
+      max-width: 7em;
+      max-height: 7em;
+      display: inline-block;
+      position: relative;
+      overflow: hidden;
+      border-radius: 50%;
+      img{
+        width: 100%;
+        height: 100%;
+      }
+    }
+  }
+  
+  
 }
 
 ul{
